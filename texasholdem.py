c4481c0fc239cbd9982d28b8d0f18ec0f5ecf4e0
Polevata <polevata@graysons-mbp.bluenet.mccallie.org>
2016-03-21 09:10:32 -0400

    Changes betting system to reflect what's actually happening and thereby fixes any folding issues.

--- texasholdem.py
+++ texasholdem.py
@@ -17,24 +17,19 @@ class TexasHoldem:
         self.theDeck.shuffle()
         self.yourHand.extend(self.theDeck.deal(2))
         self.opponentsHand.extend(self.theDeck.deal(2))
-        self.status()
         self.bet(0)
         self.middle.extend(self.theDeck.deal(3))
-        self.status()
         if self.keepBetting == True:
             self.bet(1)
-        self.middle.extend(self.theDeck.deal())
-        self.status()
         if self.keepBetting == True:
             self.bet(2)
-        self.middle.extend(self.theDeck.deal())
-        self.status()
         if self.keepBetting == True:
             self.bet(3)
-        self.showAll()
-        result = HoldemRules.results(self.yourHand,self.middle,self.opponentsHand)
-        self.endGame(result)
-
+            self.showAll()
+            result = HoldemRules.results(self.yourHand,self.middle,self.opponentsHand)
+            self.endGame(result)
+        else:
+            self.endGame()
     def status(self):
         print("Your current net worth is ${0}.".format(self.CASH))
         print("The pot is at ${0}".format(self.pot))
@@ -51,17 +46,22 @@ class TexasHoldem:
                 print()
 
     def bet(self,round):
+        self.status()
         if round == 0:
             b = int(input('Please enter a starting bid: $'))
         else:
             b = int(input('How much are you willing to raise: $'))
         if b>self.CASH:
             b=self.CASH
+            self.CASH = 0
+            self.pot += self.CASH
             print("You go all in")
+        else:
+            self.CASH -= b
+            self.pot += b
         decision = HoldemRules.goodDeal(self.opponentsHand,self.middle,self.CASH,b)
         if decision == "call":
-            self.pot += 2*b
-            self.CASH -= b
+            self.pot += b
             print("Your opponent called. The pot is now up to ${0}".format(self.pot))
         elif decision == "fold":
             self.keepBetting = False
@@ -71,17 +71,18 @@ class TexasHoldem:
             s = str(input("Your opponent raised you by ${0}! Do you wish to call or fold: ".format(decision)))
             if "call" in s or "Call" in s:
                 if decision + b > self.CASH:
-                    self.pot += 2*self.CASH
-                    self.CASH = 0
+                    self.pot += self.CASH
                     self.keepBetting = False
                     print("You both go all in and begin the wait.")
                 else:
-                    self.pot += 2*(b+decision)
-                    self.CASH -= b+decision
+                    self.pot += b + decision
+                    self.CASH -= decision
             else:
                 self.keepBetting = False
-                print("Your oppoent smiles as you step away from the table. You've lost ${0}.".format(self.pot))
+                print("Your oppoent smiles as you step away from the table.")
         print("\n")
+        if round != 0:
+            self.middle.extend(self.theDeck.deal())
 
     def showAll(self):
         self.status()
@@ -90,16 +91,18 @@ class TexasHoldem:
             for c in self.opponentsHand:
                 print(c.__str__().splitlines()[i],end=" ")
             print()
-    def endGame(self,result):
+    def endGame(self,result=""):
         print(result)
         if "tied" in result:
             self.CASH += self.pot//2
             print("The pot of ${0} was split evenly between you.".format(self.pot))
         elif "opponent" in result:
             print("The pot of ${0} was all given to your opponent.".format(self.pot))
-        else:
+        elif "won" in result:
             self.CASH += self.pot
             print("You won all ${0} in the pot!".format(self.pot))
+        else:
+            print("You lost all ${0} in the pot!".format(self.pot))
         boo = str(input("Would you like to play again? (Y/N) "))
         if "Y" in boo or "y" in boo:
             print()
